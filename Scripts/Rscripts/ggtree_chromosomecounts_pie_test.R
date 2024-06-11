37024851e82cf678f3447be48940c1eea562ae0b
Summer Blanco (she/they) <95707842+mellamosummer@users.noreply.github.com>
2024-06-11 12:31:30 -0400

    Update ggtree_chromosomecounts_pie_test.R

--- Scripts/Rscripts/ggtree_chromosomecounts_pie_test.R
+++ Scripts/Rscripts/ggtree_chromosomecounts_pie_test.R
@@ -1,91 +1,42 @@
-set.seed(2019-05-02)
-x <- rtree(30)
-
-p <- ggtree(x) + geom_tiplab()
-
-p
-
-d <- data.frame(label = x$tip.label, 
-                value = rnorm(30))
-p2 <- p + geom_facet(panel = "Dot", data = d, 
-                     geom = geom_point, mapping = aes(x = value))
-p2 + xlim_tree(6) + xlim_expand(c(-10, 10), 'Dot')
-
-
-############
-
-tree <- read.tree("/Users/summerblanco/Desktop/April2024_PhylogenomicsGeranium/TreeFiles/astral4_353_supercontigs.tree")
-p <- ggtree(tree, branch.length = "none")+ geom_tiplab() + geom_nodelab(aes(label=label,  subset=label<0.95), size=3,hjust = 1.8, vjust=2)
-p
-
-d <- read.table(file = "/Users/summerblanco/Desktop/April2024_PhylogenomicsGeranium/hybpiper_Chromosome_colordata.tsv", sep = '\t', header = TRUE)
-
-d <- d %>% select(sample_name,Chromosome.Count) %>% separate_rows(Chromosome.Count, sep =", ")
-
-d$Chromosome.Count <- as.numeric(d$Chromosome.Count)
-
-d <- d %>% rename (label = sample_name, value = Chromosome.Count)
-
-p2 <- p + geom_facet(panel = "Dot", data = d, 
-                     geom = geom_point, mapping = aes(x = value)) + xlim_expand(c(-10, 80), 'Tree') + theme_tree2() 
-
-facet_widths(p2, widths = c(2, 1))
-
-ggsave("test1.png")
-
-############
+#load tsv with new names and chromosome count data
 sample_data <- read.table(file = "/Users/summerblanco/Desktop/April2024_PhylogenomicsGeranium/hybpiper_Chromosome_colordata.tsv", sep = '\t', header = TRUE)
+
+#read in tree
 tree <- read.tree("/Users/summerblanco/Desktop/April2024_PhylogenomicsGeranium/TreeFiles/astral4_353_supercontigs.tree")
+
+#truncate LPP to only 2 sig figs
 tree$node.label <- as.numeric(tree$node.label)
 tree$node.label <- round(tree$node.label,2)
+
+#rename tips with New Names from tsv
 tree <- updateLabel(tree, sample_data$sample_name, sample_data$NewName)
-p <- ggtree(tree, branch.length = "none")%<+% sample_data + geom_tiplab(color = 'black', size = 4, geom = "text", hjust=-1 ) + geom_nodelab(aes(label=label,  subset=label<0.95), hjust=1, vjust=1)
-p
 
-d <- read.table(file = "/Users/summerblanco/Desktop/April2024_PhylogenomicsGeranium/hybpiper_Chromosome_colordata.tsv", sep = '\t', header = TRUE)
-
-d <- d %>% select(NewName,Chromosome.Count) %>% separate_rows(Chromosome.Count, sep =", ")
-
-d$Chromosome.Count <- as.numeric(d$Chromosome.Count)
-
-d <- d %>% rename (label = NewName, value = Chromosome.Count)
-
-p2 <- p + geom_facet(panel = "Chromosome Counts (2n)", data = d, 
-                     geom = geom_point, mapping = aes(x = value),alpha=0.7) + xlim_tree(23) + theme_tree2() 
-
-facet_widths(p2, widths = c(2, 1))
-
-ggsave("test2.png")
-
-############
-sample_data <- read.table(file = "/Users/summerblanco/Desktop/April2024_PhylogenomicsGeranium/hybpiper_Chromosome_colordata.tsv", sep = '\t', header = TRUE)
-tree <- read.tree("/Users/summerblanco/Desktop/April2024_PhylogenomicsGeranium/TreeFiles/astral4_353_supercontigs.tree")
-tree$node.label <- as.numeric(tree$node.label)
-tree$node.label <- round(tree$node.label,2)
-tree <- updateLabel(tree, sample_data$sample_name, sample_data$NewName)
+#plot tree cladogram with new names, node support <0.95,
 p <- ggtree(tree, branch.length = "none")%<+% sample_data + geom_tiplab(color = 'black', size = 5, geom = "text", hjust=-.1) + geom_nodelab(aes(label=label,  subset=label<0.95), hjust=1.5, vjust=-0.5, size=4)
 p
 
+#read in quartet frequencies
 dat2<-read.csv("/Users/summerblanco/Desktop/astral4_353_supercontigs_scored.tree.csv")
 
-#make pie charts
+#make pie chart insets from quartet frequencies
 dat2 <- data.frame(dat2)
 pies <- nodepie(dat2, cols=1:3)
 pietree <- inset(p, pies,width=0.08,height = 0.15)
 pietree
 
+#read in chromosome ount data and clean up 
 d <- read.table(file = "/Users/summerblanco/Desktop/April2024_PhylogenomicsGeranium/hybpiper_Chromosome_colordata.tsv", sep = '\t', header = TRUE)
-
 d <- d %>% select(NewName,Chromosome.Count) %>% separate_rows(Chromosome.Count, sep =", ")
-
 d$Chromosome.Count <- as.numeric(d$Chromosome.Count)
-
 d <- d %>% rename (label = NewName, value = Chromosome.Count)
-pietree
+
+#plot tree with chromosome counts and pie charts at nodes
 p2 <- pietree + geom_facet(panel = "Chromosome Counts (2n)", data = d, 
                      geom = geom_point, mapping = aes(x = value),alpha=0.7) + xlim_tree(23) + theme_tree()
 
 p2
+
+#make the background transparent for nice poster diagram
 p2<- p2 + theme(text=element_text(family="Helvetica"),size=5,
                        panel.background = element_rect(fill='transparent'),
                        plot.background = element_blank(),
@@ -97,12 +48,9 @@ p2<- p2 + theme(text=element_text(family="Helvetica"),size=5,
                        
                      )
 
+#make the visualization 75% tree and 25% chromosome counts vis for poster
 p3<- facet_widths(p2, widths = c(3, 1))
-ggsave(file="jtest.png")
 
-install.packages("svglite")
-library(svglite)
-svglite("test.svg")
-plot(p3,height=10*72,width=8*72)
-dev.off()
+#save as png
+ggsave(file="jtest.png")
 
